53d4c39a34a269dfcdd248295984161db741b121
Matheus Machado <matheus_lm10@hotmail.com>
2022-01-27 21:14:56 -0400

    Configures styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
+import GlobalStyle from 'styles/global'
 
 export function App(): JSX.Element {
-  return <div data-testid="greetings-container">Welcome to the Origin THA</div>
+  return (
+    <>
+      <GlobalStyle />
+      <div data-testid="greetings-container">Welcome to the Origin THA</div>
+    </>
+  )
 }
