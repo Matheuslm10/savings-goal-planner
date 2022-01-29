e8313316ab313f00f90de4f1d5ad40d857678ad2
Matheus Machado <matheus_lm10@hotmail.com>
2022-01-29 14:41:32 -0400

    style: styles the TotalAmout input.

--- src/components/TotalAmountInput/styles.ts
+++ src/components/TotalAmountInput/styles.ts
@@ -1,14 +1,5 @@
 import styled from 'styled-components'
 
-export const Input = styled.input`
-  border: 1px solid #e9eef2;
-  box-sizing: border-box;
-  border-radius: 4px;
-  height: 56px;
-  margin-top: 4px;
-  color: #4d6475;
-`
-
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
@@ -21,4 +12,33 @@ export const Container = styled.div`
     font-size: 14px;
     line-height: 150%;
   }
+`
+
+export const Input = styled.input`
+  border: 1px solid #e9eef2;
+  box-sizing: border-box;
+  border-radius: 4px;
+  height: 56px;
+  width: 100%;
+  margin-top: 4px;
+  padding: 0 13px 0 44px;
+  font-family: 'Rubik';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 120%;
+  color: #4d6475;
+  /* The icon can be found in 'assets/icons/dollar-sign.svg', here it has been URL-enconded to be used as background-image. */
+  background-image: url("data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath fill-rule='evenodd' clip-rule='evenodd' d='M12 0a1 1 0 0 1 1 1v22a1 1 0 1 1-2 0V1a1 1 0 0 1 1-1z' fill='%23CBD5DC'/%3E%3Cpath fill-rule='evenodd' clip-rule='evenodd' d='M6.318 5.318A4.5 4.5 0 0 1 9.5 4H17a1 1 0 1 1 0 2H9.5a2.5 2.5 0 1 0 0 5h5a4.5 4.5 0 1 1 0 9H6a1 1 0 1 1 0-2h8.5a2.5 2.5 0 0 0 0-5h-5a4.5 4.5 0 0 1-3.182-7.682z' fill='%23CBD5DC'/%3E%3C/svg%3E");
+  background-repeat: no-repeat;
+  background-position: left 13px center;
+
+  /* Removes spinners buttons in Chrome, Safari, Edge, Opera */
+  ::-webkit-outer-spin-button,
+  ::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+  /* Removes spinners buttons in Firefox */
+  -moz-appearance: textfield;
 `
