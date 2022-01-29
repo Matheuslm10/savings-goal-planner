8e9cb6eb1d276ecd8af514a4732f8b55237e2a63
Matheus Machado <matheus_lm10@hotmail.com>
2022-01-29 16:57:04 -0400

    feat: adds mask currency to the amount input.

--- src/components/TotalAmountInput/index.tsx
+++ src/components/TotalAmountInput/index.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 import * as S from './styles'
 
+import { currency } from 'utils/currencyMask'
+
 const TotalAmountInput = () => {
+  const [amount, setAmount] = useState('')
+
+  const handleChange = useCallback((e: React.FormEvent<HTMLInputElement>) => {
+    setAmount(currency(e).currentTarget.value)
+  }, [])
+
   return (
     <S.Container>
       <label htmlFor="total-amount">Total amount</label>
-      <S.Input type="number" id="total-amount" name="total-amount" min="0" />
+      <S.Input
+        type="text"
+        inputMode="decimal"
+        id="total-amount"
+        name="total-amount"
+        value={amount}
+        onChange={handleChange}
+      />
     </S.Container>
   )
 }
