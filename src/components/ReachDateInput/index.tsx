c8fe56bb0f62f8f09f1a39c7e6da4d370efdcefc
Matheus Machado <matheus_lm10@hotmail.com>
2022-01-29 20:51:49 -0400

    feat: creates the ReachDate input layout structure and styles it.

--- src/components/ReachDateInput/index.tsx
+++ src/components/ReachDateInput/index.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import * as S from './styles'
 
+import { ReactComponent as LeftArrowIcon } from 'assets/icons/left-arrow.svg'
+import { ReactComponent as RightArrowIcon } from 'assets/icons/right-arrow.svg'
+
 const ReachDateInput = () => {
   return (
     <S.Container>
       <label htmlFor="reach-date">Reach goal by</label>
-      <S.Input id="reach-date" name="reach-date" />
+      <S.Wrapper id="reach-date" role="input">
+        <button>
+          <LeftArrowIcon />
+        </button>
+        <S.Text>
+          <S.Month>January</S.Month>
+          <S.Year>2022</S.Year>
+        </S.Text>
+        <button>
+          <RightArrowIcon />
+        </button>
+      </S.Wrapper>
     </S.Container>
   )
 }
--- src/components/ReachDateInput/styles.ts
+++ src/components/ReachDateInput/styles.ts
@@ -1,13 +1,46 @@
 import styled from 'styled-components'
 
-export const Input = styled.input`
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   border: 1px solid #e9eef2;
   box-sizing: border-box;
   border-radius: 4px;
   height: 56px;
   width: 192px;
   margin-top: 4px;
+  padding: 0 20px;
   color: #4d6475;
+
+  button {
+    border: 0;
+    background: none;
+    appearance: none;
+    width: 24px;
+    height: 34px;
+  }
+`
+
+export const Text = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+export const Month = styled.div`
+  font-style: normal;
+  font-weight: 600;
+  font-size: 16px;
+  color: #1e2a32;
+`
+
+export const Year = styled.div`
+  font-style: normal;
+  font-size: 16px;
+  margin-top: 4px;
+  color: #708797;
 `
 
 export const Container = styled.div`
